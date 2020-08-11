9bbdb5dae4490c77bb0731f4ac9b4c582fc92c89
saroj-nf <saroj.k@newfangled.io>
2020-08-11 17:57:54 +0530

    Updates

---
+++ precache-manifest.3b143a0beced01d176c14a9a7b2e4b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1efaefe8394f450a17297351d00408",
+    "url": "/index.html"
+  },
+  {
+    "revision": "196930730dbe7fb27473",
+    "url": "/static/css/main.930fbcce.chunk.css"
+  },
+  {
+    "revision": "06b60d482b44fff2af1d",
+    "url": "/static/js/2.7526f2f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7526f2f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "196930730dbe7fb27473",
+    "url": "/static/js/main.b3704b31.chunk.js"
+  },
+  {
+    "revision": "43b25eb14975ab1b1919",
+    "url": "/static/js/runtime-main.146b4aeb.js"
+  }
+]);
